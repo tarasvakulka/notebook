6b568d72bfb2454af62bd32e2c3d985caf5f9d81
Taras Vakulka <vakulkataras@gmail.com>
2018-03-29 18:41:53 +0300

    add structure

---
+++ src/app/components/note-editor/note-editor.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-note-editor',
+  templateUrl: './note-editor.component.html',
+  styleUrls: ['./note-editor.component.scss']
+})
+export class NoteEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
